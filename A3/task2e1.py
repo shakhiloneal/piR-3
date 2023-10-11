db7e4fe8ff5a2b5d0edafa967107faa408670921
shakhiloneal <akboda988@gmail.com>
2023-10-11 16:52:15 +1100

    A3 created

---
+++ A3/task2e1.py
@@ -0,0 +1,31 @@
+"""
+A3 Task 2, part 1. Evaluation of e1
+"""
+#def functions
+def evaluate_exp1(exp1: list):
+    term1 = exp1[1]
+    term2 = exp1[2]
+
+    result = term1 / term2
+    return result
+
+def evaluate_exp2(exp2: list):
+    term1 = exp2[1]
+    term2 = exp2[2]
+
+    result = term1 + term2
+    return result
+
+#begin main
+full_exp = ['*', ['/', 4, 2], ['+', 8, 7]]
+exp1 = full_exp[1]
+exp2 = full_exp[2]
+
+#evaluatefull_exp[1]
+exp1Result = evaluate_exp1(exp1)
+#evaluatefull_exp[2]
+exp2Result = evaluate_exp2(exp2)
+
+#evaluate and print full sum
+finalResult = exp1Result * exp2Result
+print(finalResult)
